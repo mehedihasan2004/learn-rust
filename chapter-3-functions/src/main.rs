15e5d04f8a32cb8643fd002ce454b90d73e015c7
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-08-09 22:37:35 +0600

    chapter-3: expression

--- chapter-3-functions/src/main.rs
+++ chapter-3-functions/src/main.rs
@@ -4,6 +4,20 @@ fn main() {
     hello_world();
     tell_height(180);
     human("Mehedi", 21, 180.01);
+
+    let x: u8 = {
+        let price: u8 = 5;
+        let qty: u8 = 10;
+
+        price * qty
+    };
+
+    println!("Result is: {}", x);
+
+    let y: u8 = add(4, 6);
+
+    println!("Value from variable 'y' is: {}.", y);
+    println!("Value from function 'add' is: {}.", add(40, 60));
 }
 
 fn hello_world() {
@@ -20,3 +34,19 @@ fn human(name: &str, age: u8, height: f32) {
         name, age, height
     );
 }
+
+// Function returning values
+fn add(a: u8, b: u8) -> u8 {
+    a + b
+}
+
+/*
+= = = = = = EXPRESSIONS = = = = = =
+Expressions: Anything that returns a value.
+Like: -
+5
+true & false
+add(3, 4)
+if condition {value1} else {value2}
+{()}
+*/
