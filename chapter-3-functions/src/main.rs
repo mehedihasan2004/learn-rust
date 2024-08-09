cf37f874b9f069b158f172fa2335aa18836ad46c
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-08-09 22:50:58 +0600

    chapter-3: statement

--- chapter-3-functions/src/main.rs
+++ chapter-3-functions/src/main.rs
@@ -18,6 +18,12 @@ fn main() {
 
     println!("Value from variable 'y' is: {}.", y);
     println!("Value from function 'add' is: {}.", add(40, 60));
+
+    let weight: f64 = 70.0;
+    let height: f64 = 1.80;
+    let bmi: f64 = calculate_bmi(weight, height);
+
+    println!("Your BMI is: {:.2}", bmi);
 }
 
 fn hello_world() {
@@ -49,4 +55,23 @@ if condition {value1} else {value2}
 // Function returning values
 fn add(a: u8, b: u8) -> u8 {
     a + b
+}
+
+/*
+= = = = = = STATEMENTS = = = = = =
+Statement: Anything that does not return a value.
+Almost all statements in Rust end with ;
+
+let y = let x = 10;
+
+1. Variable declarations: let x = 5;
+2. Function definitions: fn foo() {}
+3. Control flow statements: if condition { /* code */} else { /* code */}, while condition { /* code */}, etc.
+*/
+
+// Final example
+
+fn calculate_bmi(weight_kg: f64, height_m: f64) -> f64 {
+    weight_kg / (height_m * height_m)
+    // Formula: BMI = height(kg)/height(m)^2
 }
