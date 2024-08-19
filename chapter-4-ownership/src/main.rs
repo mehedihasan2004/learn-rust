85d9c5218e6e34ddf7d874a19327e24cfdcf2e83
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-08-19 18:11:48 +0600

    chapter-4: ownership rule number 1

---
+++ chapter-4-ownership/src/main.rs
@@ -0,0 +1,28 @@
+/*
+Ownership
+[stopping/Resuming the programm]
+
+Ownership intorduced by Rust to solve memory safety issues and high performance at the same time.
+
+Q1. What is Ownership?
+Ans: Every value has a single owner [every variale has one value, and it is it's sole owner].
+
+Ownership Rules:-
+1. Each value in Rust has a variable that's it's owner.
+2. There can be only one owner at a time.
+3. When the owner goes out of scope, the value will be dropped.
+*/
+
+/*
+Example of rule number 1.
+fn main() {
+    let s1 = String::from("RUST");
+    let len = calculate_length(&s1);
+
+    println!("Length of '{}' is {}.", s1, len);
+}
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
+*/
