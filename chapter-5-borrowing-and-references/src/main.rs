022efc6dcac5ab944dd8afdc68d473c36bc3d935
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-09-23 23:39:57 +0600

    chapter-5: borrowing & references

--- chapter-5-borrowing-and-references/src/main.rs
+++ chapter-5-borrowing-and-references/src/main.rs
@@ -21,12 +21,48 @@ Create reference by add "&".
 
 // II. Mutable reference
 fn main() {
-    let mut x: i8 = 5;
-    let y: &mut i8 = &mut x;
+    // let mut x: i8 = 5;
+    // let y: &mut i8 = &mut x;
 
-    *y += 1;
-    *y -= 3;
+    // *y += 1;
 
-    println!("Value of x is: {}", x);
+    // println!("Value of x is: {}", x);
     // println!("Value of y is: {}", y); // Error-> A mutable reference is allowed only.
+
+    let mut account: BankAccount = BankAccount {
+        owner: "John".to_string(),
+        balance: 149.44,
+    };
+
+    // Immutable borrow to check the balance
+    account.check_balance();
+
+    // Mutable borrow to withdrao money
+    account.withdraw(45.5);
+
+    // Immutable borrow to check the balance
+    account.check_balance();
+}
+
+struct BankAccount {
+    owner: String,
+    balance: f64,
+}
+
+impl BankAccount {
+    fn withdraw(&mut self, amount: f64) {
+        println!(
+            "Withdrawing {} from account, owned by {}",
+            amount, self.owner
+        );
+
+        self.balance -= amount;
+    }
+
+    fn check_balance(&self) {
+        println!(
+            "Account owned by {}, has a balance of {}",
+            self.owner, self.balance
+        );
+    }
 }
