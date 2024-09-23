7d77a14ba930b265a86f38e5c56b5235152526d3
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-09-24 00:02:36 +0600

    chapter-7: constants

---
+++ chapter-7-constants/src/main.rs
@@ -0,0 +1,15 @@
+const A: u8 = 55;
+const PI: f64 = 3.141592653;
+const ONE_HOUR_IN_SECONDS: u32 = 60 * 60;
+
+fn main() {
+    let mut x: u8 = 4;
+
+    const X: u8 = 5;
+
+    println!("The value of A is: {}", A);
+    println!("The value of PI is: {}", PI);
+    println!("One hour in seconds: {}", ONE_HOUR_IN_SECONDS);
+    println!("The value of x is: {}", x);
+    println!("The value of X is: {}", X);
+}
